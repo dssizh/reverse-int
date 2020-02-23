ba45f64caa594c6baef21612123118aab611adfd
Dmitriy S <sutyginds@gmail.com>
2020-02-23 20:03:08 +0400

    init: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let result = "";
+  for (i=str.length-1; i>=0; i--) {
+    result = result + str[i];
+  }
+//   console.log(result);
+  return Number.parseInt(result);
 }
